614ea115e3f122c7d66bff6bf1147a727f672e6f
Nikita <chuvaaash@gmail.com>
2025-02-26 17:09:34 +0300

    feat broadcast

--- src/app/broadcast-channel.service.ts
+++ src/app/broadcast-channel.service.ts
@@ -1,55 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+interface ChatMessage {
+  username: string;
+  text: string;
+  time: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class BroadcastChannelService {
-  private channel: BroadcastChannel | null = null;
+export class BroadcastChannelService implements OnDestroy {
+  private channel: BroadcastChannel;
+  private messageSubject = new BehaviorSubject<ChatMessage | null>(null);
+  public messages$: Observable<ChatMessage | null> = this.messageSubject.asObservable();
 
   constructor() {
     this.channel = new BroadcastChannel('chat-channel');
-
-    navigator.serviceWorker.addEventListener('message', (event) => {
-      if (event.data && event.data.type === 'NEW_MESSAGE') {
-        try {
-          const parsedMessage = JSON.parse(event.data.message);
-          this.channel?.postMessage(parsedMessage);
-        } catch (error) {
-          console.error('Ошибка парсинга JSON:', error);
-        }
+    this.channel.onmessage = (event) => {
+      try {
+        const parsedMessage: ChatMessage = JSON.parse(event.data);
+        this.messageSubject.next(parsedMessage);
+      } catch (error) {
+        console.error('Ошибка парсинга JSON:', error);
       }
-    });
+    };
   }
 
-  sendMessage(message: { username: string; text: string; time: string }) {
+  sendMessage(message: ChatMessage) {
     const jsonMessage = JSON.stringify(message);
-    if (this.channel) {
-      this.channel.postMessage(jsonMessage);
-    }
-
-    navigator.serviceWorker.controller?.postMessage({
-      type: 'NEW_MESSAGE',
-      message: jsonMessage
-    });
+    this.channel.postMessage(jsonMessage);
   }
 
-  listenMessages(callback: (message: { username: string; text: string; time: string}) => void) {
-    if (this.channel) {
-      this.channel.onmessage = (event) => {
-        try {
-          const parsedMessage = JSON.parse(event.data);
-          callback(parsedMessage);
-        } catch (error) {
-          console.error('Ошибка парсинга JSON:', error);
-        }
-      };
-    }
+  listenMessages(callback: (message: ChatMessage) => void) {
+    this.messages$.subscribe((message) => {
+      if (message) callback(message);
+    });
   }
 
   closeChannel() {
-    if (this.channel) {
-      this.channel.close();
-      this.channel = null;
-    }
+    this.channel.close();
+  }
+
+  ngOnDestroy(): void {
+    this.closeChannel();
   }
 }
